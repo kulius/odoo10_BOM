2bc5f767e763c2a56ffaa6956f4939170e78e153
Pengguancheng <kimey583@gmail.com>
2017-04-06 16:15:15 +0800

    2017.4.6

---
+++ odoo10addons_bom/bom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "",
+    'version': '1.0',
+    'depends': [
+    ],
+    'author': "alltop,Peng,Zoe",
+    'website': "http://eoffice.alltop.com.tw",
+    'category': '',
+    'description': """
+    """,
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
